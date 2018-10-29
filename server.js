c079d421103328a2887f5774156373cb83e5014a
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-28 19:55:32 -0700

    created react app client

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const
     mongoose = require('mongoose'),
     MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/put-it-on',
     PORT=3001;
+    usersRoutes = require('./routes/users.js'),
+	
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
     console.log(err || `Connected to MongoDB.`)
@@ -16,9 +18,13 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
 app.use(logger('dev'))
 app.use(express.json())
 
+
 app.get('/pio', (req, res) => {
-	res.json({message: "Put It On root."})
+    res.json({message: "Put It On root."})
 })
+
+app.use('/pio/users', usersRoutes)
+
 
 app.listen(PORT, (err) => {
 	console.log(err || `Server running on port ${PORT}.`)
