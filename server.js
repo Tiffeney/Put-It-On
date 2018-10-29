bcb65f0f11bc758c06eb3a0c1f3c3c0a52b5e206
Tiffeney <tiffeney.rambo@gmail.com>
2018-10-28 19:30:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const 
+    express = require('express'),
+    app = express(),
+    logger = require('morgan'),
+    mongoose = require('mongoose'),
+    MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/put-it-on',
+    PORT=3001;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+    console.log(err || `Connected to MongoDB.`)
+})
+
+
+app.use(logger('dev'))
+app.use(express.json())
+
+app.get('/pio', (req, res) => {
+	res.json({message: "Put It On root."})
+})
+
+app.listen(PORT, (err) => {
+	console.log(err || `Server running on port ${PORT}.`)
+})
